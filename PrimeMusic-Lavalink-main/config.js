213675acdb345e919b3e241c4f5a568ceb3ea88e
Uchiha_Drago.edtz 炎像 <redbirdgaming5@gmail.com>
2025-11-07 12:39:39 +0300

    Update config.js

--- PrimeMusic-Lavalink-main/config.js
+++ PrimeMusic-Lavalink-main/config.js
@@ -4,23 +4,23 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  mongodbUri : "mongodb+srv://advaithbalasubramaniyan:boruto76@advaith.ao77u9c.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "85aab1d51a174aad9eed6d7989f530e6",
+  spotifyClientSecret : "b2ad05aa725e434c88776a1be8eab6c2",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityName: "Youtube Music", 
+  activityType: "PLAYING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glace",
-      host: "87.106.62.92",
-      port:  11642,
+      name: "Sakura",
+      password: "Rikka",
+      host: "lavalink.aiko-project.xyz",
+      port:  2333,
       secure: false
     }
   ]
